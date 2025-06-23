2005305c12d2d835931503916abbe22f30ed1855
Henrique-Kriguer <henriquekriguer@gmail.com>
2025-06-23 19:43:56 -0300

    configurando rotas com lazyLoading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [MatToolbarModule, MatIconModule, MatSlideToggleModule],
+  imports: [MatToolbarModule, MatIconModule, MatSlideToggleModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
