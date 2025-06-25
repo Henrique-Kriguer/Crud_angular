ccfa1ec823f338c0d6739e94b205507f98bf490a
Henrique-Kriguer <henriquekriguer@gmail.com>
2025-06-24 21:24:47 -0300

    ajuste modulo app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CoursesComponent } from './courses/courses/courses.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'courses', pathMatch: 'full' },
