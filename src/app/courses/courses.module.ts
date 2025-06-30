2da84c557aa8a45b7700f9f6b024a606647b983a
Henrique-Kriguer <henriquekriguer@gmail.com>
2025-06-30 17:49:30 -0300

    Exibindo tabela de cursos

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
 import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses/courses.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
+    MatTableModule,
+    MatCardModule
+
   ]
 })
 export class CoursesModule { }
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
+  ],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+  courses: Course[] = [
+    {_id:'1',name:'Angular',category: 'front-end'},
+    {_id:'2',name:'Spring Boot',category: 'back-end'},
+    {_id:'3',name:'Java',category: 'back-end'},
+    {_id:'4',name:'JavaScript',category: 'front-end'},
+    {_id:'5',name:'TypeScript',category: 'front-end'}
 
+  ];
+  displayedColumns: string[] = ['name', 'category'];
 }
